ed06a63e1a5369e35b41d8ef1dcbed3e58b976c1
tlagbayani <tlagbayani@gmail.com>
2020-01-14 21:51:17 +0000

    Listing All the Events

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#index"
+  resources :events, only: [:index, :show]
   namespace :administrator do
     resources :events, only: [:new, :create, :show]
   end
